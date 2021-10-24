2527bffbc58075c6ca9787095999390251738d1c
Bohdan Tereta <bohdan.tereta@gmail.com>
2021-10-24 18:11:55 +0300

    final layer

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 # docker create -ti --name echarts-ssr bash
-docker build -t echarts-ssr ./functions/get-chart   
+docker build -t echarts-ssr ./functions/get-chart
 docker rm  echarts-ssr       
 docker create --name echarts-ssr echarts-ssr
 rm -rf ./output
 mkdir ./output
 mkdir ./output/nodejs/
 mkdir ./output/nodejs/node14
-mkdir ./output/nodejs/lib
+mkdir ./output/lib
 docker cp echarts-ssr:/var/task/node_modules ./output/nodejs/node14
-# docker cp echarts-ssr:/ ./output/lib
-cd ./output && zip -rm node_modules.zip ./nodejs
-
+docker cp echarts-ssr:/usr/lib64/libuuid.so ./output/lib/libuuid.so
+docker cp echarts-ssr:/usr/lib64/libuuid.so.1 ./output/lib/libuuid.so.1
+docker cp echarts-ssr:/usr/lib64/libuuid.so.1.3.0 ./output/lib/libuuid.so.1.3.0
+docker cp echarts-ssr:/usr/lib64/libmount.so.1 ./output/lib/libmount.so.1
+docker cp echarts-ssr:/usr/lib64/libmount.so.1.1.0 ./output/lib/libmount.so.1.1.0
+docker cp echarts-ssr:/usr/lib64/libblkid.so.1 ./output/lib/libblkid.so.1
+docker cp echarts-ssr:/usr/lib64/libblkid.so.1.1.0 ./output/lib/libblkid.so.1.1.0
+cd ./output 
+zip -rm node_modules.zip .
--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
-  Prerequisites:
+  # Prerequisites:
   install serverless with `npm install -g serverless`
   switch to node version 14 (optional) vian nvm `nvm use 14`
-
-  How to run 
+  # How to run 
+  For layer version (default), run ./build.sh first to build layer
   deploy: `serverless deploy`
   test: `curl -X POST {your url}`
   remove: `serverless remove`
-   
+  # Useful links: 
+  https://medium.com/appgambit/part-2-create-lambda-layers-with-serverless-framework-and-offline-support-ad2a5a8dabfb
+  http://blog.maxieduncan.co.nz/aws/2021/07/07/lambda-container-image-support
+  # Some statistics
+  Docker init duration: 11 seconds
+  Layer init duration 1 second
+  Response time under 1 second in both cases after warmed
+  Layer size 40+ MB
+  Lambda size 40+ KB
