5bdea89d2a462c1ade10cf88a7344d6b3009095f
Bohdan Tereta <bohdan.tereta@gmail.com>
2021-10-24 16:42:52 +0300

    layer but libso error

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 # docker create -ti --name echarts-ssr bash
-docker build -t echarts-ssr .   
+docker build -t echarts-ssr ./functions/get-chart   
 docker rm  echarts-ssr       
 docker create --name echarts-ssr echarts-ssr
-rm -rf ./node_modules
-docker cp echarts-ssr:/var/task/node_modules ./
+rm -rf ./output
+mkdir ./output
+mkdir ./output/nodejs/
+mkdir ./output/nodejs/node14
+mkdir ./output/nodejs/lib
+docker cp echarts-ssr:/var/task/node_modules ./output/nodejs/node14
+# docker cp echarts-ssr:/ ./output/lib
+cd ./output && zip -rm node_modules.zip ./nodejs
 
